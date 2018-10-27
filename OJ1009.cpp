40920f2c645c9a32ce5d20213e725c55252bef61
chenxi <jsjliyang@gmail.com>
2018-10-27 21:21:25 +0800

    Update OJ1009.cpp

--- OJ1009.cpp
+++ OJ1009.cpp
@@ -2,30 +2,20 @@
 #include <stdio.h>
 using namespace std;
 
-
-double buy(int hand,int price)
+double dealwith(int hand,int price,int flag)
 {
     int dealnum = hand*100;
     double totalprice = dealnum*price*1.0;//乘以1.0转换为double
 
-    double sxf = max(totalprice*0.002,5.0);
-    double trans = dealnum/1000.0;
-    double communication = 1.0;
-
-    return totalprice + sxf + trans + communication;
-}
-
-double sale(int hand,int price)
-{
-    int dealnum = hand*100;
-
-    double totalprice = dealnum*price*1.0;//乘以1.0转换为double
     double sxf = max(totalprice*0.002,5.0);
     double trans = dealnum/1000.0;
     double communication = 1.0;
     double tax = totalprice*0.001;
 
-    return totalprice - sxf - trans - communication - tax;
+    if(flag == 1)
+        return (-1.0)*(totalprice + sxf + trans + communication);
+    else
+        return totalprice - (sxf + trans + communication + tax);
 }
 
 
@@ -33,10 +23,11 @@ int main()
 {
     int m,n;
     double income = 0.00;
+    int i,j;
 
     cin>>m;
     int deal[m][3];
-    for(int i = 0; i < m;i++)
+    for(i = 0; i < m;i++)
     {
         cin>>deal[i][0];
         cin>>deal[i][1];
@@ -45,66 +36,28 @@ int main()
 
     cin>>n;
     int price[n][2];
-    for(int i = 0;i < n;i++)
+    for(j = 0;j < n;j++)
     {
-        cin>>price[i][0];
-        cin>>price[i][1];
+        cin>>price[j][0];
+        cin>>price[j][1];
     }
 
     int p = 0;
-    int flag = 0;
-    for(int i = 0;i<m;i++)
+
+    for(i = 0;i < m;i++)
     {
-        int j = 1;
-        for(int j = 1;j < n;j++)
+        for(j = 1;j < n;j++)
         {
-            if(deal[i][0] <= price[j][0] && deal[i][0] >= price[j-1][0])
-                {
-                    p = price[j-1][1];
-                    flag = deal[i][2];
-                    /*if(deal[i][2] == 1)
-                    {
-                        double buynum = buy(deal[i][1],price[j-1][1]);
-                        income -= buynum;
-                        break;
-                    }
-                    else if(deal[i][2] == 2)
-                        {
-                            double salenum = sale(deal[i][1],price[j-1][1]);
-                            income += salenum;
-                            break;
-                        }*/
-                }
-            else
+            if(deal[i][0] < price[j][0] && deal[i][0] >= price[j-1][0])
             {
+                p = price[j-1][1];
+                break;
+            }
+            else
                 p = price[j][1];
-                flag = deal[i][2];
-                    /*if(deal[i][2] == 1)
-                    {
-                        double dealnum = buy(deal[i][1],price[j][1]);
-                        income -= dealnum;
-                        break;
-                    }
-                    else if(deal[i][2] == 2)
-                        {
-                            double dealnum = sale(deal[i][1],price[j][1]);
-                            income += dealnum;
-                            break;
-                        }*/
-            }
-            if(flag == 1)
-            {
-                double dealnum = buy(deal[i][1],p);
-                income -= dealnum;
-                break;
-            }
-            else
-            {
-                double dealnum = sale(deal[i][1],p);
-                income += dealnum;
-                break;
-            }
         }
+        double dealnum = dealwith(deal[i][1],p,deal[i][2]);
+        income += dealnum;
     }
 
     printf("%.2f\n",income);
